bf6cfd127399c1d1ff5ee1b732029229ca4dbb63
Alexander Matyushentsev <AMatyushentsev@gmail.com>
2025-11-29 10:38:02 -0800

    typescript
    Signed-off-by: Alexander Matyushentsev <AMatyushentsev@gmail.com>

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import MermaidWater from './components/mermaid_water';
+
+function App(): React.JSX.Element {
+  return (
+    <div className="app">
+      <MermaidWater />
+    </div>
+  );
+}
+
+export default App;
+
