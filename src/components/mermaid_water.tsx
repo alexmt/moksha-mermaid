9b8c294b38f7905956349b3f00386601314e2e7a
Alexander Matyushentsev <AMatyushentsev@gmail.com>
2025-11-29 11:04:15 -0800

    background
    Signed-off-by: Alexander Matyushentsev <AMatyushentsev@gmail.com>

--- src/components/mermaid_water.tsx
+++ src/components/mermaid_water.tsx
@@ -1,8 +1,9 @@
 import mermaidImage from '../assets/mermaid.png';
+import backgroundImage from '../assets/background.png';
 
 function MermaidWater(): React.JSX.Element {
   return (
-    <div className="background">
+    <div className="background" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <div className="mermaid-container">
         <img src={mermaidImage} alt="Mermaid" className="mermaid-image" />
       </div>
