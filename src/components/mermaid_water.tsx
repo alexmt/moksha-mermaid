14d2dc5c9ab176bffc5f8738ff351c10d11b4049
Alexander Matyushentsev <AMatyushentsev@gmail.com>
2025-11-29 10:52:51 -0800

    better image/webpack
    Signed-off-by: Alexander Matyushentsev <AMatyushentsev@gmail.com>

--- src/components/mermaid_water.tsx
+++ src/components/mermaid_water.tsx
@@ -1,26 +1,11 @@
-import mermaidImage from '../assets/mermaid.svg';
+import mermaidImage from '../assets/mermaid.png';
 
 function MermaidWater(): React.JSX.Element {
   return (
     <div className="background">
-      <div className="water"></div>
       <div className="mermaid-container">
         <img src={mermaidImage} alt="Mermaid" className="mermaid-image" />
       </div>
-      <svg className="water-filter">
-        <filter id="turbulence" x="0" y="0" width="100%" height="100%">
-          <feTurbulence id="sea-filter" numOctaves="3" seed="2" baseFrequency="0.02 0.05"></feTurbulence>
-          <feDisplacementMap scale="20" in="SourceGraphic"></feDisplacementMap>
-          <animate
-            xlinkHref="#sea-filter"
-            attributeName="baseFrequency"
-            dur="60s"
-            keyTimes="0;0.5;1"
-            values="0.02 0.06;0.04 0.08;0.02 0.06"
-            repeatCount="indefinite"
-          ></animate>
-        </filter>
-      </svg>
     </div>
   );
 }
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[contenthash].js',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 1234,
+    hot: true,
+    open: true,
+  },
+};
+
